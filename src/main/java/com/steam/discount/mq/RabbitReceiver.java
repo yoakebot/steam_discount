ed47f6abea70a4d251a9eaa8e0e47f3e1d5317b0
yoake <yoakext@outlook.com>
2025-08-02 00:36:04 +0800

    使用mq死信队列实现延迟消息

--- src/main/java/com/steam/discount/mq/RabbitReceiver.java
+++ src/main/java/com/steam/discount/mq/RabbitReceiver.java
@@ -19,8 +19,9 @@ public class RabbitReceiver {
             exchange = @Exchange(
                     value = MQConstants.EXCHANGE_TEST
             ),
-            key = MQConstants.ROUTING_TEST
-    ))
+            key = MQConstants.ROUTING_TEST),
+            ackMode = "AUTO"
+    )
     public void receiveTest(@Payload Boy boy, @Header(AmqpHeaders.CONSUMER_QUEUE) String queue) {
         System.out.println("【收到延迟消息】ID：" + boy.time() + "，来源队列=" + queue);
     }
@@ -28,8 +29,9 @@ public class RabbitReceiver {
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = MQConstants.QUEUE_DEAD_LETTER, durable = "true"),
             exchange = @Exchange(value = MQConstants.EXCHANGE_DEAD_LETTER, type = ExchangeTypes.DIRECT, durable = "true"),
-            key = MQConstants.ROUTING_KEY_DEAD_LETTER
-    ))
+            key = MQConstants.ROUTING_KEY_DEAD_LETTER),
+            ackMode = "AUTO"
+    )
     public void receiveDead(@Payload Boy boy, @Header(AmqpHeaders.CONSUMER_QUEUE) String queue) {
         System.out.println("【收到延迟消息】ID：" + boy.time() + "，来源队列=" + queue);
     }
